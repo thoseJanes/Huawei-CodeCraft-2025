f212bed75de3aea299c09a95ab156becea428e18
unknown <tyutzhouzijian@163.com>
2025-03-13 15:03:12 +0800

    Add log tools

--- code/main.cpp
+++ code/main.cpp
@@ -1,35 +1,16 @@
 #include <cstdio>
 #include <cassert>
 #include <cstdlib>
+#include "global.h"
+#include "operation.h"
 
-#define MAX_DISK_NUM (10 + 1)
-#define MAX_DISK_SIZE (16384 + 1)
-#define MAX_REQUEST_NUM (30000000 + 1)
-#define MAX_OBJECT_NUM (100000 + 1)
-#define REP_NUM (3)
-#define FRE_PER_SLICING (1800)
-#define EXTRA_TIME (105)
-
-typedef struct Request_ {
-    int object_id;
-    int prev_id;
-    bool is_done;
-} Request;
-
-typedef struct Object_ {
-    int replica[REP_NUM + 1];
-    int* unit[REP_NUM + 1];
-    int size;
-    int last_request_point;
-    bool is_delete;
-} Object;
 
 Request request[MAX_REQUEST_NUM];
 Object object[MAX_OBJECT_NUM];
 
-int T, M, N, V, G;
-int disk[MAX_DISK_NUM][MAX_DISK_SIZE];
-int disk_point[MAX_DISK_NUM];
+extern int T, M, N, V, G;
+extern int disk[MAX_DISK_NUM][MAX_DISK_SIZE];
+extern int disk_point[MAX_DISK_NUM];
 
 void timestamp_action()
 {
