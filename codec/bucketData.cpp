7ec8e517220b2b4b1b790990bba106b66b53831d
Eleanor <dlutzhouzijian@163.com>
2025-04-02 15:38:22 +0100

    Adjust structure

--- codec/bucketData.cpp
+++
@@ -1,9 +0,0 @@
-#include "bucketData.h"
-
-int** HistoryBucket::delSta = nullptr;
-int** HistoryBucket::reqSta = nullptr;
-int** HistoryBucket::wrtSta = nullptr;
-
-int** StatisticsBucket::delSta = nullptr;
-int** StatisticsBucket::reqSta = nullptr;
-int** StatisticsBucket::wrtSta = nullptr;
